c1e907bf37250e3d87f86c471bad2d79a66b2d3d
654530249@qq.com <654530249@qq.com>
2019-12-24 16:03:51 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -2,12 +2,30 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// 如果引入的是 index.js，可以使用下面的简写，等价于 import router from './router/index.js'
+import router from './router'
+import './directives'
+import './components'
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
+
+// 使用插件
+Vue.use(VueSweetalert2)
+Vue.use(Message)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  // 注入 store
+  store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created() {
+    console.log(this.$options.el) // => '#app'
+  }
 })
---
+++ src/router/routes.js
@@ -0,0 +1,61 @@
+export default [
+  {
+    path: '/auth/register',
+    name: 'Register',
+    component: () => import('@/views/auth/Register')
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  {
+    path: '*',
+    redirect: '/'
+  },
+  {
+    path: '/auth/login',
+    name: 'Login',
+    component: () => import('@/views/auth/Login')
+  },
+  // EditUsers
+  {
+    path: '/users/1/edit',
+    name: 'EditUsers',
+    component: () => import('@/views/users/Edit'),
+    children: [
+      {
+        path: '',
+        name: 'EditProfile',
+        component: () => import('@/views/users/Profile'),
+        // auth 为 true，标识当前路由需要登录才能访问
+        meta: { auth: true }
+      },
+      // EditAvatar
+      {
+        path: '/users/1/edit_avatar',
+        name: 'EditAvatar',
+        component: () => import('@/views/users/Avatar'),
+        meta: { auth: true }
+      },
+      // EditPassword
+      {
+        path: '/users/1/edit_password',
+        name: 'EditPassword',
+        component: () => import('@/views/users/Password'),
+        meta: { auth: true }
+      }
+    ]
+  },
+  {
+    path: '/articles/create',
+    name: 'Create',
+    component:()=> import('@/views/articles/Create'),
+    meta: { auth: true }
+  },
+  {
+    path: '/articles/:articleId/content',
+    name: 'Content',
+    component:()=> import('@/views/articles/Content')
+  }
+]
