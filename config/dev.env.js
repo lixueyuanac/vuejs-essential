5d3aa2f67b5447a29d6307fb90c9768b3a9c184d
654530249@qq.com <654530249@qq.com>
2020-01-19 17:18:49 +0800

    修改配置文件

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development2222"',
-  VUE_APP_API: '"http://144.34.160.177:8000/"',
+  VUE_APP_API: '"http://localhost:8000/"',
   VUE_APP_JWT_KEY: '"token"',
 })
--- config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  VUE_APP_API: '"http://144.34.160.177:8000/"',
-  VUE_APP_JWT_KEY: '"token"',
-}
