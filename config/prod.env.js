5dbb7b8d009c00ed31fb189806c84b9c975a0a40
654530249@qq.com <654530249@qq.com>
2020-01-19 17:06:24 +0800

    修改请求url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_API: '"http://localhost:8000/"',
+  VUE_APP_API: '"http://144.34.160.177:8000/"',
   VUE_APP_JWT_KEY: '"token"',
 }
