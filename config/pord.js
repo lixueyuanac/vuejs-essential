d34eaeda835a48c99f55e2dd9c8e31f37b2cd51d
lixueyuanac <654530249@qq.com>
2020-01-29 13:57:42 +0800

    ip

--- config/pord.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  VUE_APP_API: '"http://144.34.160.177:8000/"',
-  VUE_APP_JWT_KEY: '"token"',
-})
